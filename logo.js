80269968f127d982d4a47107e13fafcd95fca454
Gabrielli <114274029+gabriellicristina@users.noreply.github.com>
2024-10-18 11:25:36 -0300

    Create logo.js

---
+++ logo.js
@@ -0,0 +1,7 @@
+## cria uma sessão dentro no fórmulário e inclui um campo de 'notes'
+## dentro da nota insere: '<div> <br></div>'
+
+## fluxo: '[nome formulário] > Criado ou editado > Carregamento do formulário > logo'
+
+ImageURL = "endereço_da_imagem";
+input.plain = "<img src =\"" + ImageURL + "\" style=\"width: 150px; height: auto;\"></img";
